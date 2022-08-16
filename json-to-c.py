f64e14883bb70a1e6deb1a2eb8bcac2233aaa04e
boxerbomb <nathanhenry1999@gmail.com>
2022-08-16 14:31:48 -0400

    Added cb_prefixed instructions

--- json-to-c.py
+++ json-to-c.py
@@ -40,8 +40,27 @@ REGISTER_LIST = [
 "AF",
 "BC",
 "DE",
-"HL"
+"HL",
+"d8",
+"d16",
+"a8",
+"a16",
+"r8",
+"00H",
+"08H",
+"10H",
+"18H",
+"20H",
+"28H",
+"30H",
+"38H",
+"NC",
+"C",
+"NZ",
+"Z"
 ]
+
+
   
 # Stores Unique mnemonics
 unprefixed_mnemonic_list = []
@@ -91,6 +110,12 @@ for i in data['unprefixed']:
 				break
 			operand0_name_index = operand0_name_index + 1
 
+		if operand_0_reg_id == 99:
+			print("")
+			print("")
+			print("ZZZZZZZZZZZZZZZZ: "+operand0_name)
+			print("")
+			print("")
 
 		try:
 			op_0_bytes = operand0_obj['bytes']
@@ -156,6 +181,106 @@ for i in data['unprefixed']:
 
 	print("(*unprefixed_list)["+str(unprefixed_count)+"] = *new_inst("+str(opcode)+","+str(mne_ID)+","+str(bytes_num)+","+str(cycles)+","+operand0_string+","+operand1_string+","+immediate_string+","+bin(flag_set)+","+bin(flag_reset)+");")
 	unprefixed_count = unprefixed_count +1
+
+cbprefixed_count = 0
+for i in data['cbprefixed']:
+	obj = data['cbprefixed'][i]
+
+	opcode = i
+
+	mne_index = 0
+	mne_ID = 99
+	for mne_item in cb_mnemonic_list:
+		if mne_item==data['cbprefixed'][i]['mnemonic']:
+			mne_ID = mne_index
+			break
+		mne_index = mne_index + 1
+	
+	bytes_num = data['cbprefixed'][i]['bytes']
+	cycles = data['cbprefixed'][i]['cycles'][0]
+
+	operands_list = data['cbprefixed'][i]['operands']
+	operand0_string="NULL"
+	operand1_string="NULL"
+
+	if len(operands_list)>=1:
+		operand0_obj = data['cbprefixed'][i]['operands'][0]
+		op_0_bytes = 0
+
+		operand0_name = data['cbprefixed'][i]['operands'][0]['name']
+		operand_0_reg_id = 99
+		operand0_name_index = 0
+		for item in REGISTER_LIST:
+			if operand0_name.upper()==item.upper():
+				operand_0_reg_id = operand0_name_index
+				break
+			operand0_name_index = operand0_name_index + 1
+
+
+		try:
+			op_0_bytes = operand0_obj['bytes']
+		except:
+			op_0_bytes = 0
+
+		operand0_string = "new_operand("+str(operand_0_reg_id)+","+str(op_0_bytes)+","
+		if operand0_obj['immediate']==True:
+			operand0_string = operand0_string + "true"
+		else:
+			operand0_string = operand0_string + "false"
+		operand0_string = operand0_string + ")"
+
+	if len(operands_list)>=2:
+		operand1_obj = data['cbprefixed'][i]['operands'][1]
+		op_1_bytes = 0
+
+		try:
+			op_1_bytes = operand1_obj['bytes']
+		except:
+			op_1_bytes = 0
+
+		operand1_name = data['cbprefixed'][i]['operands'][1]['name']
+		operand_1_reg_id = 99
+		operand1_name_index = 0
+		for item in REGISTER_LIST:
+			if operand1_name.upper()==item.upper():
+				operand_1_reg_id = operand1_name_index
+				break
+			operand1_name_index = operand1_name_index + 1
+
+		operand1_string = "new_operand("+str(operand1_name_index)+","+str(op_1_bytes)+","
+		if operand1_obj['immediate']==True:
+			operand1_string = operand1_string + "true"
+		else:
+			operand1_string = operand1_string + "false"
+		operand1_string = operand1_string + ")"
+
+	if data['cbprefixed'][i]['immediate']==True:
+		immediate_string = "true"
+	else:
+		immediate_string = "false"
+
+	flag_set = 0
+	if data['cbprefixed'][i]['flags']['Z'] == '1':
+		flag_set = flag_set | 0b10000000 
+	if data['cbprefixed'][i]['flags']['N'] == '1':
+		flag_set = flag_set | 0b01000000
+	if data['cbprefixed'][i]['flags']['H'] == '1':
+		flag_set = flag_set | 0b00100000
+	if data['cbprefixed'][i]['flags']['C'] == '1':
+		flag_set = flag_set | 0b00010000
+
+	flag_reset = 0
+	if data['cbprefixed'][i]['flags']['Z'] == '0':
+		flag_reset = flag_reset | 0b10000000 
+	if data['cbprefixed'][i]['flags']['N'] == '0':
+		flag_reset = flag_reset | 0b01000000
+	if data['cbprefixed'][i]['flags']['H'] == '0':
+		flag_reset = flag_reset | 0b00100000
+	if data['cbprefixed'][i]['flags']['C'] == '0':
+		flag_reset = flag_reset | 0b00010000
+
+	print("(*cbprefixed_list)["+str(cbprefixed_count)+"] = *new_inst("+str(opcode)+","+str(mne_ID)+","+str(bytes_num)+","+str(cycles)+","+operand0_string+","+operand1_string+","+immediate_string+","+bin(flag_set)+","+bin(flag_reset)+");")
+	cbprefixed_count = cbprefixed_count +1
 	
 
 # Closing file
