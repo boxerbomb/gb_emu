d3a50c0a3491ac67f881aef2ac1271ee352fd21c
nathan <nathan@debian-BULLSEYE-live-builder-AMD64>
2022-08-15 21:37:06 -0400

    Decided to use C, I didn't like the flow of object oriented code

--- read_file.c
+++ read_file.c
@@ -1,4 +1,6 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 // Reads entire cartidge file into cart_buffer
 void readCart(unsigned char** cart_buffer){
@@ -31,26 +33,13 @@ void read_data_from_cart(unsigned char** cart_buffer, unsigned char** variable, 
 		(*variable)[i] = (*cart_buffer)[i+location];
 	}
 }
-void read_data_from_cart(unsigned char** cart_buffer, char** variable, unsigned int location, unsigned int size){
-	for(int i=0; i<size; i++){
-		(*variable)[i] = (*cart_buffer)[i+location];
-	}
-}
-
-void read_data_from_cart(unsigned char** cart_buffer, bool &variable, unsigned int location, unsigned int size=0){
-		if ((*cart_buffer)[location] == 0x00){
-			variable = false;
-		}else{
-			variable = true;
-		}
-}
 
 
 struct cart_metadata {
 	unsigned char* entry_point; 	// Often NOP JP 0x150
 	unsigned char* logo; 		    // Logo data
 	unsigned char* title; 				// The Title
-	bool cgb;						// Gameboy Color?
+	unsigned char* cgb;						// Gameboy Color?
 	// New Licencsee Code? Not sure what this is 0x144-0x145
 	bool sgb;						// Super Gameboy
 	unsigned char* cartidge_type;
@@ -63,7 +52,7 @@ struct cart_metadata {
 	unsigned char* global_checksum;
 };
 
-void read_cart_metadata(unsigned char** cart_buffer, cart_metadata *metadata){
+void read_cart_metadata(unsigned char** cart_buffer, struct cart_metadata *metadata){
 
 	// Allocate Memory in the passed struct and then assign it data
 	metadata->entry_point = (unsigned char*) malloc (sizeof(unsigned char)*4);
@@ -75,8 +64,8 @@ void read_cart_metadata(unsigned char** cart_buffer, cart_metadata *metadata){
 	metadata->title = (unsigned char*) malloc (sizeof(unsigned char)*15);
 	read_data_from_cart(cart_buffer, &metadata->title, 0x134, 15);
 
-	//Just a boolean, no need to allocate memory
-	read_data_from_cart(cart_buffer, metadata->cgb, 0x143);
+	metadata->cgb= (unsigned char*) malloc (sizeof(unsigned char)*1);
+	read_data_from_cart(cart_buffer, &metadata->cgb, 0x143,1);
 
 	//New Licensee Code could go here
 
@@ -105,34 +94,34 @@ void read_cart_metadata(unsigned char** cart_buffer, cart_metadata *metadata){
 	read_data_from_cart(cart_buffer, &metadata->global_checksum,0x14E,2);
 }
 
-void printMetadata(cart_metadata *metadata){
-	std::cout<<"Entry Point: 0x"<<std::hex<<(unsigned int)metadata->entry_point[3]<<(unsigned int)metadata->entry_point[2]<<std::endl;
+void printMetadata(struct cart_metadata *metadata){
+	printf("Entry Point 0x%X%X\n",(unsigned int)metadata->entry_point[3],(unsigned int)metadata->entry_point[2]);
 
-	std::cout<<"Game Title: ";
+	printf("Game Title: ");
 	int index=0;
 	while(metadata->title[index] != 0x00){
-		std::cout<<metadata->title[index];
+		printf("%c",metadata->title[index]);
 		index++;
 	}
-	std::cout<<std::endl;
+	printf("\n");
 
-	std::cout<<"Color? ";
-	if(metadata->cgb == 0x00){
-		std::cout<<"No"<<std::endl;
-	}else if(metadata->cgb == 0x80){
-		std::cout<<"Supported"<<std::endl;
-	}else if(metadata->cgb == 0xC0){
-		std::cout<<"Required"<<std::endl;
+	printf("Color? ");
+	if(*metadata->cgb == 0x00){
+		printf("No\n");
+	}else if(*metadata->cgb == 0x80){
+		printf("Supported\n");
+	}else if(*metadata->cgb == 0xC0){
+		printf("Required\n");
 	}
 
-	std::cout<<"More metadata available"<<std::endl;
+	printf("More metadata available\n");
 }
 
 
 int main () {
 
   unsigned char * cart_buffer;
-  cart_metadata metadata;
+  struct cart_metadata metadata;
 
   readCart(&cart_buffer);
   read_cart_metadata(&cart_buffer,&metadata);
